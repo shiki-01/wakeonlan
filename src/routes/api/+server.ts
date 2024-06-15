a8e036948134995a08db46490b2030e17024de76
shiki-01 <0110shiki@gmail.com>
2024-06-15 12:36:27 +0900

    Update +server.ts

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -17,15 +17,12 @@ export async function POST({ request }: { request: Request }) {
     return new Response(JSON.stringify({ message: 'Invalid or missing MAC address' }), { status: 400 });
   }
 
-  const safeGlobalIP = globalIP || '';
-  const safeDeviceIP = deviceIP || globalIP || '255.255.255.255';
+  const targetIP = globalIP || deviceIP || '255.255.255.255';
   const safePort = port || '9';
 
-  console.log(`Sending magic packet to ${safeDeviceIP} on port ${safePort} with MAC address ${mac}`);
-
   try {
-    await createMagicPacket(mac, safeGlobalIP, parseInt(safePort, 10), safeDeviceIP);
-    return new Response(JSON.stringify({ message: `Magic packet sent successfully \n Sending magic packet to ${safeDeviceIP} on port ${safePort} with MAC address ${mac}` }), { status: 200 });
+    await createMagicPacket(mac, targetIP, parseInt(safePort, 10));
+    return new Response(JSON.stringify({ message: `Magic packet sent successfully` }), { status: 200 });
   } catch (error) {
     console.error(error);
     return new Response(JSON.stringify({ message: 'An error occurred' }), { status: 500 });
